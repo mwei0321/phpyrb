e4d7165b5961a9e86f9b67a3ba4304f9386b2d64
mwei0321 <491518132@qq.com>
2014-09-01 18:33:32 +0800

    Update layer.js
    css

--- layer.js
+++ layer.js
@@ -4,89 +4,71 @@
  * @home http://www.phpyrb.com
  */
 var layer = {};
-
-;(function ($,window,document,underfined){
+;(function($, window, document, underfined) {
 	//
-	
 	//浏览器宽度
-	var SreenW = $(document).width();
+	var SreenW = $(window).width();
 	// //浏览器高度
 	var SreenH = $(window).height();
-	// alert(SreenH +'=>'+SreenW);
-	
+	 //alert(SreenH +'=>'+SreenW);
 	//初始化配置
 	var Options = {
-		bgOpacity   : '0.8',
-		bgColor     : '#666',
-		EditW		: 400,
-		EditH		: 600,
-		ThumbW		: 100,
-		ThumbH		: 100,
+		bgOpacity : '0.8',
+		bgColor : '#a1a1a1',
+		EditW : 400,
+		EditH : 600,
+		ThumbW : 100,
+		ThumbH : 100,
 	}
-	
-	$.fn.extend(layer,{
-		box:function(title,btn){
-			var h = '<div class="seach_select">'+
-                     '<form action="" method="post">'+
-                       ' <input type="text" name="uid" class="intext" value="" placeholder="用户ID"/>'+
-                        '<input type="text" name="keyword" class="intext" value="" placeholder="用户名或邮箱"/>'+
-                        '<input type="submit" class="btn btn-sm btn-primary" value="搜索"　/>'+
-                        '</form>'+
-                    '</div>';
-			outbox('aaaa',h);
-//			close();
+	$.fn.extend(layer, {
+		box : function(title, btn) {
+			var h = '<div class="seach_select">' + '<form action="" method="post">' + ' <input type="text" name="uid" class="intext" value="" placeholder="用户ID"/>' + '<input type="text" name="keyword" class="intext" value="" placeholder="用户名或邮箱"/>' + '<input type="submit" class="btn btn-sm btn-primary" value="搜索"　/>' + '</form>' + '</div>';
+			outbox('aaaa', h);
+			close();
 		},
-		
-		error:function(msg){
+		error : function(msg) {
 			message(msg);
 		},
-		
-		success:function(msg){
-			message(msg,1);
+		success : function(msg) {
+			message(msg, 1);
 		},
 	});
-	
-	function message(msg,type,time){
-		var clas = type ? 'success' : 'error'; 
-		var html = '<span class="'+clas+'">'+msg+'</span>';
-		outbox(null,html);
+	function message(msg, type, time) {
+		var clas = type ? 'success' : 'error';
+		var html = '<span class="' + clas + '">' + msg + '</span>';
+		outbox(null, html);
 		close();
 	}
-	
-	function close(time){
+
+	function close(time) {
 		var t = time ? time : 1.5;
-		setTimeout(function(){
-			$('#outsidebox').fadeOut('1000',function(){
+		setTimeout(function() {
+			$('#outsidebox').fadeOut('1000', function() {
 				$(this).remove();
 				$('#mwbg').remove();
 			});
-		},t*1000);
-	}
-	
-	function outbox(title,ht,oboxw){
-		var t = title ? '<h3>'+title+'</h3>' : '';
-		var html = '<div id="mwbg"></div>'+
-					'<div id="outsidebox">'+t+ht+'<div class="clr></div></div>';
-		$('body').append(html);
-		var w = oboxw ? oboxw : $('#outsidebox').width();
-		alert(w);
-		w = w ? w : $('#outsidebox').find('span').width();
-		style(w);
+		}, t * 1000);
 	}
 
-	function style(oboxw){
+	function outbox(title, ht, oboxw) {
+		var t = title ? '<h3>' + title + '</h3>' : '';
+		var html = '<div id="mwbg"></div>' + '<div id="outsidebox">' + t + ht + '<div class="clr></div></div>';
+		$('body').append(html);
+		style();
+	}
+
+	function style() {
+		var oboxw = $('#outsidebox').width();
 		var oboxh = $('#outsidebox').height();
-		var oboxl = $('#outsidebox').css({'left':(SreenW-oboxw)/2+'px','top':(SreenH-oboxh)/2+'px'})
-		var style = '<style>#mwbg{width:'+SreenW+'px;height:'+SreenH+'px;display:block;position:fixed;z-index:900;opacity: 0.8;filter:Alpha(Opacity=90);postion:fixed;background:'+Options.bgColor+';top:0;left:0;}'+
-					'#outsidebox{border:5px solid #333;background:#fff;border-radius:10px;z-index:990;position:fixed;padding:10px;} #outsidebox h3{height:35px;line-height:35px;border-bottom:1px solid #ccc;background:#efefef;}'+
-					'#outsidebox .layermsg{font-size:14px;} #outsidebox .layermsg .error{color:red;} #outsidebox .layermsg .success{color:green;} '+
-					'#outsidebox .clr{clear:both;padding:0;margin:0;}'+
-					'</style>';
-		
+		var oboxl = $('#outsidebox').css({
+			'left' : (SreenW - oboxw) / 2 + 'px',
+			'top' : (SreenH - oboxh) / 2 + 'px'
+		})
+		var style = '<style>#mwbg{width:' + SreenW + 'px;height:' + SreenH + 'px;display:block;position:fixed;z-index:900;opacity: 0.8;filter:Alpha(Opacity=90);postion:fixed;background:' + Options.bgColor + ';top:0;left:0;}' + 
+		'#outsidebox{border:5px solid #666;background:#fff;border-radius:10px;z-index:990;position:fixed;padding:10px;} #outsidebox h3{height:35px;line-height:35px;border-bottom:1px solid #ccc;background:#efefef;}' +
+		 '#outsidebox {} #outsidebox .error{color:red;font-size:14px;} #outsidebox .success{color:green;font-size:14px;} ' + '#outsidebox .clr{clear:both;padding:0;margin:0;}' + 
+		 '</style>';
 		$("head").append(style);
 	}
-	
-})(jQuery,window,document)
 
-
-
+})(jQuery, window, document)
